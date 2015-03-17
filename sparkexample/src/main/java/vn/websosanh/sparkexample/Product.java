81f08cc03e6af4517fd7aa2c4319e32c083d4382
BaoHV <BaoHV@WIN-6ICNIQVFE0A>
2015-03-17 15:30:15 +0700

    commit

--- sparkexample/src/main/java/vn/websosanh/sparkexample/Product.java
+++ sparkexample/src/main/java/vn/websosanh/sparkexample/Product.java
@@ -4,33 +4,47 @@ import java.io.Serializable;
 import java.text.MessageFormat;
 
 public class Product implements Serializable {
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = -5838232291344432959L;
 	private Integer id;
-    private String name;
-    private Integer parent;
+	private String name;
+	private String parents;
 
-    public Product() { }
+	public Product() {
+	}
 
-    public Product(Integer id, String name, Integer parent) {
-        this.id = id;
-        this.name = name;
-        this.parent = parent;
-    }
+	public Product(Integer id, String name, String parents) {
+		this.id = id;
+		this.name = name;
+		this.parents = parents;
+	}
 
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
+	public Integer getId() {
+		return id;
+	}
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public Integer getParent() { return parent; }
-    public void setParents(Integer parent) { this.parent = parent; }
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public String toString() {
-        return MessageFormat.format("Product'{'id={0}, name=''{1}'', parents={2}'}'", id, name, parent);
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getParents() {
+		return parents;
+	}
+
+	public void setParent(String parent) {
+		this.parents = parent;
+	}
+
+	@Override
+	public String toString() {
+		return MessageFormat.format(
+				"Product'{'id={0}, name=''{1}'', parents={2}'}'", id, name,
+				parents);
+	}
 }
