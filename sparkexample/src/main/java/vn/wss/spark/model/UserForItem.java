fd0c1d79f0f1f9761f15cf87d84c6ba47e951721
BaoHV <BaoHV@WIN-6ICNIQVFE0A>
2015-03-24 11:58:04 +0700

    dasdas

--- sparkexample/src/main/java/vn/wss/spark/model/UserForItem.java
+++ sparkexample/src/main/java/vn/wss/spark/model/UserForItem.java
@@ -1,6 +1,7 @@
 package vn.wss.spark.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class UserForItem implements Serializable {
 	/**
@@ -8,12 +9,12 @@ public class UserForItem implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	private Long idtem;
-	private Iterable<Long> listuser;
+	private List<Long> listuser;
 
 	public UserForItem() {
 	}
 
-	public UserForItem(Long idtem, Iterable<Long> listuser) {
+	public UserForItem(Long idtem, List<Long> listuser) {
 		this.idtem = idtem;
 		this.listuser = listuser;
 	}
@@ -26,12 +27,11 @@ public class UserForItem implements Serializable {
 		this.idtem = idtem;
 	}
 
-	public Iterable<Long> getListuser() {
+	public List<Long> getListuser() {
 		return listuser;
 	}
 
-	public void setListuser(Iterable<Long> listuser) {
+	public void setListuser(List<Long> listuser) {
 		this.listuser = listuser;
 	}
-
 }
