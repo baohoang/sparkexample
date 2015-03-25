b01117b1b3c95e4643ffc3b4a162691e69132ddc
BaoHV <BaoHV@WIN-6ICNIQVFE0A>
2015-03-25 13:51:37 +0700

    asdasd

--- sparkexample/src/main/java/vn/wss/spark/recommendation/CassandraConnection.java
+++ sparkexample/src/main/java/vn/wss/spark/recommendation/CassandraConnection.java
@@ -9,6 +9,8 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 import org.apache.spark.api.java.function.Function;
 
+import vn.wss.spark.model.Tracking;
+
 import com.datastax.spark.connector.japi.CassandraRow;
 
 import static com.datastax.spark.connector.japi.CassandraJavaUtil.javaFunctions;
@@ -23,15 +25,15 @@ public class CassandraConnection {
 			JavaSparkContext sc = new JavaSparkContext(conf);
 	    // entire table as an RDD
 	    // assumes your table test was created as CREATE TABLE test.kv(key text PRIMARY KEY, value int);
-	    JavaRDD<String> data = javaFunctions(sc).cassandraTable("tracking" , "tracking").map(new Function<CassandraRow, String>() {
+	    JavaRDD<String> data = javaFunctions(sc).cassandraTable("tracking" , "tracking").where("year_month = ?", 201501).map(new Function<CassandraRow, String>() {
 
 			@Override
 			public String call(CassandraRow v1) throws Exception {
 				// TODO Auto-generated method stub
+				
 				return v1.toString();
 			}
 		});
-	    
 	    logger.info("completed ..."+ data.count());
 	    sc.stop();
 	    // print some basic stats
