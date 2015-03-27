980fc59fa5f3bb6c86f8fba5f5d1eb610234b432
BaoHV <BaoHV@WIN-6ICNIQVFE0A>
2015-03-27 10:14:41 +0700

    dasdasd

--- sparkexample/src/main/java/vn/wss/spark/recommendation/WordSearch.java
+++ sparkexample/src/main/java/vn/wss/spark/recommendation/WordSearch.java
@@ -36,44 +36,53 @@ public class WordSearch {
 				"spark.cassandra.connection.host", "10.0.0.11");
 
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		JavaPairRDD<String, Integer> data = javaFunctions(sc)
-				.cassandraTable("tracking", "tracking").select("uri")
-				.filter(new Function<CassandraRow, Boolean>() {
+		javaFunctions(sc).cassandraTable("tracking", "tracking").select("uri").map(new Function<CassandraRow,String>() {
 
-					@Override
-					public Boolean call(CassandraRow v1) throws Exception {
-						// TODO Auto-generated method stub
-						if (v1.contains("uri")) {
-							String uri = v1.getString("uri");
-							logger.info(uri);
-							if (uri.startsWith("http://websosanh.vn/s/")) {
-								return true;
-							}
-						}
-						return false;
-					}
-				}).mapToPair(new PairFunction<CassandraRow, String, Integer>() {
-
-					@Override
-					public Tuple2<String, Integer> call(CassandraRow t)
-							throws Exception {
-						// TODO Auto-generated method stub
-						String uri = t.getString("uri");
-						return new Tuple2<String, Integer>(StringUtils
-								.getWordSearch(uri), 1);
-					}
-				});
-		JavaPairRDD<String, Integer> map = data
-				.reduceByKey(new Function2<Integer, Integer, Integer>() {
-
-					@Override
-					public Integer call(Integer v1, Integer v2)
-							throws Exception {
-						// TODO Auto-generated method stub
-						return v1 + v2;
-					}
-				});
-		logger.info("count :" + map.count());
+			@Override
+			public String call(CassandraRow v1) throws Exception {
+				// TODO Auto-generated method stub
+				logger.info(v1.toString());
+				return v1.toString();
+			}
+		}).saveAsTextFile("/spark/wordsearch");;
+//		JavaPairRDD<String, Integer> data = javaFunctions(sc)
+//				.cassandraTable("tracking", "tracking").select("uri")
+//				.filter(new Function<CassandraRow, Boolean>() {
+//
+//					@Override
+//					public Boolean call(CassandraRow v1) throws Exception {
+//						// TODO Auto-generated method stub
+//						if (v1.contains("uri")) {
+//							String uri = v1.getString("uri");
+//							logger.info(uri);
+//							if (uri.startsWith("http://websosanh.vn/s/")) {
+//								return true;
+//							}
+//						}
+//						return false;
+//					}
+//				}).mapToPair(new PairFunction<CassandraRow, String, Integer>() {
+//
+//					@Override
+//					public Tuple2<String, Integer> call(CassandraRow t)
+//							throws Exception {
+//						// TODO Auto-generated method stub
+//						String uri = t.getString("uri");
+//						return new Tuple2<String, Integer>(StringUtils
+//								.getWordSearch(uri), 1);
+//					}
+//				});
+//		JavaPairRDD<String, Integer> map = data
+//				.reduceByKey(new Function2<Integer, Integer, Integer>() {
+//
+//					@Override
+//					public Integer call(Integer v1, Integer v2)
+//							throws Exception {
+//						// TODO Auto-generated method stub
+//						return v1 + v2;
+//					}
+//				});
+//		logger.info("count :" + map.count());
 
 		// String path = "/home/hdspark/wordsearch";
 		//
