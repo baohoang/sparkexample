4fc75ff3d2492a513cbc4003fcab09f28c9b5d9c
BaoHV <BaoHV@WIN-6ICNIQVFE0A>
2015-03-30 12:35:28 +0700

    dasda

--- sparkexample/src/main/java/vn/wss/spark/recommendation/PreProcessData.java
+++ sparkexample/src/main/java/vn/wss/spark/recommendation/PreProcessData.java
@@ -35,7 +35,7 @@ public class PreProcessData {
 		CassandraJavaRDD<TrackingModel> rawData = javaFunctions(sc)
 				.cassandraTable("tracking", "tracking",
 						mapRowTo(TrackingModel.class)).select("uri", "user_id");
-		logger.info(rawData.count());
+		logger.info("count "+rawData.count());
 		JavaRDD<PModel> data = rawData.filter(
 				new Function<TrackingModel, Boolean>() {
 
