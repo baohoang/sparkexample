2258850f80e176a8ef058f2f34edd80dabd149ab
BaoHV <BaoHV@WIN-6ICNIQVFE0A>
2015-03-30 16:08:27 +0700

    dasda

--- sparkexample/src/main/java/vn/wss/spark/model/PModel.java
+++ sparkexample/src/main/java/vn/wss/spark/model/PModel.java
@@ -5,22 +5,49 @@ import java.io.Serializable;
 public class PModel implements Serializable {
 	private long userID;
 	private long itemID;
+
 	public PModel() {
 	}
+
 	public PModel(long userID, long itemID) {
 		this.userID = userID;
 		this.itemID = itemID;
 	}
+
 	public long getUserID() {
 		return userID;
 	}
+
 	public void setUserID(long userID) {
 		this.userID = userID;
 	}
+
 	public long getItemID() {
 		return itemID;
 	}
+
 	public void setItemID(long itemID) {
 		this.itemID = itemID;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		if (obj instanceof PModel) {
+			PModel d = (PModel) obj;
+			return d.getUserID() == userID && d.getItemID() == itemID;
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		int prime = 31;
+		int result = 1;
+		result = result * prime + Long.hashCode(userID);
+		result = result * prime + Long.hashCode(itemID);
+		return result;
+	}
+
 }
--- sparkexample/src/main/java/vn/wss/spark/recommendation/PreProcessData.java
+++ sparkexample/src/main/java/vn/wss/spark/recommendation/PreProcessData.java
@@ -3,21 +3,23 @@ package vn.wss.spark.recommendation;
 import static com.datastax.spark.connector.japi.CassandraJavaUtil.javaFunctions;
 import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapRowTo;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.SQLContext;
 
-import scala.Tuple2;
 import vn.wss.spark.model.PModel;
 import vn.wss.spark.model.TrackingModel;
 
@@ -27,7 +29,7 @@ public class PreProcessData {
 	private static final Logger logger = LogManager
 			.getLogger(CassandraConnection.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SparkConf conf = new SparkConf(true).set(
 				"spark.cassandra.connection.host", "10.0.0.11");
 
@@ -61,30 +63,20 @@ public class PreProcessData {
 				long itemid = Long.parseLong(itemIDStr);
 				return new PModel(userid, itemid);
 			}
-		});
-		JavaPairRDD<Long, Long> data2 = raw
-				.mapToPair(new PairFunction<TrackingModel, Long, Long>() {
-
-					@Override
-					public Tuple2<Long, Long> call(TrackingModel v1)
-							throws Exception {
-						// TODO Auto-generated method stub
-						long userid = Long.parseLong(v1.getUser_id());
-						String regex = ".*\\/([0-9]+)\\/so-sanh.htm";
-						Pattern pattern = Pattern.compile(regex);
-						Matcher matcher = pattern.matcher(v1.getUri());
-						String itemIDStr = "-1";
-						if (matcher.matches()) {
-							itemIDStr = matcher.group(1);
-						}
-						long itemid = Long.parseLong(itemIDStr);
-						return new Tuple2<Long, Long>(userid, itemid);
-					}
-				}).di;
+		}).distinct();
 		SQLContext sqlContext = new SQLContext(sc);
 		DataFrame schemaPeople = sqlContext.createDataFrame(data, PModel.class);
 		logger.info("create table completed ...");
+//		schemaPeople.registerTempTable(tableName);
+		Configuration configuration = new Configuration();
+		FileSystem hdfs = FileSystem.get(URI.create("hdfs://master:9000"),
+				configuration);
+		if (hdfs.exists(new Path("/spark/rawdata/parquet"))) {
+			hdfs.delete(new Path("/spark/rawdata/parquet"), true);
+		}
 		schemaPeople.saveAsParquetFile("/spark/rawdata/parquet");
+//		schemaPeople.insertInto(tableName);
+//		schemaPeople.saveAsTable(tableName, mode);
 		sc.stop();
 	}
 }
