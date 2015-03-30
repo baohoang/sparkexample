a92a3db5b255b6fb454ad0ba5c9463e4854f7ca2
BaoHV <BaoHV@WIN-6ICNIQVFE0A>
2015-03-30 14:24:05 +0700

    asdasd

--- sparkexample/src/main/java/vn/wss/spark/recommendation/PreProcessData.java
+++ sparkexample/src/main/java/vn/wss/spark/recommendation/PreProcessData.java
@@ -20,6 +20,7 @@ import vn.wss.spark.model.TrackingModel;
 
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.SaveMode;
 
 import com.datastax.spark.connector.japi.rdd.CassandraJavaRDD;
 
@@ -65,57 +66,8 @@ public class PreProcessData {
 		SQLContext sqlContext = new SQLContext(sc);
 		DataFrame schemaPeople = sqlContext.createDataFrame(data,
 				PModel.class);
-		schemaPeople.registerTempTable("pairmodel");
 		logger.info("create table completed ...");
-		// Calendar calendar = Calendar.getInstance();
-		// calendar.set(2015, 1, 1, 0, 0, 0);
-		// Date date = calendar.getTime();
-		// Date now = new Date();
-		// while (!date.after(now)) {
-		// // Date d1 = DateUtils.getStartOfDay(date);
-		// // Date d2 = DateUtils.getEndOfDay(date);
-		// Calendar c = Calendar.getInstance();
-		// c.setTime(date);
-		// int year = c.get(Calendar.YEAR);
-		// int month = c.get(Calendar.MONTH);
-		// int day = c.get(Calendar.DAY_OF_MONTH);
-		// int year_month = year * 100 + month;
-		// String path = "/spark/" + year + "/" + month + "/" + day;
-		// String d1 = year + "-" + month + "-" + day + " " + "0:0:0+0700";
-		// String d2 = year + "-" + month + "-" + (day + 1) + " "
-		// + "0:0:0+0700";
-		// JavaPairRDD<LongWritable, LongWritable> data = rawData
-		// .where("year_month = ? AND at > ? AND at < ?", year_month,
-		// d1, d2)
-		// .mapToPair(
-		// new PairFunction<CassandraRow, LongWritable, LongWritable>() {
-		//
-		// @Override
-		// public Tuple2<LongWritable, LongWritable> call(
-		// CassandraRow t) throws Exception {
-		// // TODO Auto-generated method stub
-		// String userIdStr = t.getString("user_id");
-		// String uri = t.getString("uri");
-		// String itemIdStr = StringUtils
-		// .getItemIDStr(uri);
-		// if (userIdStr != null && itemIdStr != null) {
-		// LongWritable userid = new LongWritable(
-		// Long.parseLong(userIdStr));
-		// LongWritable itemid = new LongWritable(
-		// Long.parseLong(itemIdStr));
-		// return new Tuple2<LongWritable, LongWritable>(
-		// userid, itemid);
-		// }
-		// return null;
-		// }
-		// });
-		// // logger.info(date.toString() + " has completed with " +
-		// // data.count());
-		//
-		// data.saveAsHadoopFile(path, LongWritable.class, LongWritable.class,
-		// SequenceFileOutputFormat.class);
-		// date = DateUtils.addDays(date, 1);
-		// }
+		schemaPeople.saveAsParquetFile("/spark/rawdata/parquet");
 		sc.stop();
 	}
 }
