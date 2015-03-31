fcc1a6168f2dbf10969540039e0beee7cc8b5bc7
BaoHV <BaoHV@WIN-6ICNIQVFE0A>
2015-03-31 19:17:06 +0700

    adasd

--- sparkexample/src/main/java/vn/wss/spark/recommendation/SaveData.java
+++ sparkexample/src/main/java/vn/wss/spark/recommendation/SaveData.java
@@ -39,9 +39,8 @@ public class SaveData {
 					@Override
 					public Tuple2<Long, Long> call(Row t) throws Exception {
 						// TODO Auto-generated method stub
-						logger.info(t.toString());
-						long key = t.getLong(1);
-						long val = t.getLong(0);
+						long key = t.getLong(0);
+						long val = t.getLong(1);
 						return new Tuple2<Long, Long>(key, val);
 					}
 				});
