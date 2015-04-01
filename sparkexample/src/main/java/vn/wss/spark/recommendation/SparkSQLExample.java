90bba6576bbfb8d43a780d304ba06192419c2801
BaoHV <BaoHV@WIN-6ICNIQVFE0A>
2015-04-01 16:20:43 +0700

    dasda

--- sparkexample/src/main/java/vn/wss/spark/recommendation/SparkSQLExample.java
+++ sparkexample/src/main/java/vn/wss/spark/recommendation/SparkSQLExample.java
@@ -6,13 +6,16 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 
+import scala.Tuple2;
 import vn.wss.spark.model.PModel;
 
 public class SparkSQLExample {
@@ -23,31 +26,38 @@ public class SparkSQLExample {
 		SparkConf conf = new SparkConf();
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		SQLContext sqlContext = new SQLContext(sc);
-		List<PModel> a=new ArrayList<PModel>();
-		a.add(new PModel(0, 1));
-		a.add(new PModel(0, 2));
-		a.add(new PModel(0, 3));
-		a.add(new PModel(0, 4));
-		List<PModel> b=new ArrayList<PModel>();
-		b.add(new PModel(0, 1));
-		b.add(new PModel(0, 2));
-		b.add(new PModel(0, 3));
-		b.add(new PModel(0, 4));
-		JavaRDD<PModel> x1=sc.parallelize(a);
-		JavaRDD<PModel> x2=sc.parallelize(b);
-		JavaRDD<PModel> s=x1.union(x2);
-		DataFrame d=sqlContext.createDataFrame(s, PModel.class);
-		d.show();
-//		DataFrame data = sqlContext.load("/spark/rawdata/parquet");
-//		//get from raw data 
-//		data.toJavaRDD().foreach(new VoidFunction<Row>() {
-//			
-//			@Override
-//			public void call(Row t) throws Exception {
-//				// TODO Auto-generated method stub
-//				logger.info(t.toString());
-//			}
-//		});
+		List<Tuple2<Integer, PModel>> a = new ArrayList<Tuple2<Integer, PModel>>();
+		a.add(new Tuple2<Integer, PModel>(0, new PModel(0, 1)));
+		a.add(new Tuple2<Integer, PModel>(1, new PModel(0, 2)));
+		a.add(new Tuple2<Integer, PModel>(2, new PModel(0, 3)));
+		List<Tuple2<Integer, Integer>> b = new ArrayList<Tuple2<Integer, Integer>>();
+		b.add(new Tuple2<Integer, Integer>(0, 1));
+		b.add(new Tuple2<Integer, Integer>(1, 1));
+		JavaPairRDD<Integer, PModel> x1 = sc.parallelizePairs(a);
+		JavaPairRDD<Integer, Integer> x2 = sc.parallelizePairs(b);
+		JavaPairRDD<Integer, Tuple2<PModel, Integer>> x = x1.join(x2);
+		long count = x
+				.filter(new Function<Tuple2<Integer, Tuple2<PModel, Integer>>, Boolean>() {
+
+					@Override
+					public Boolean call(
+							Tuple2<Integer, Tuple2<PModel, Integer>> v1)
+							throws Exception {
+						// TODO Auto-generated method stub
+						return v1._2()._2() == 1;
+					}
+				}).count();
+		logger.info(count);
+		// DataFrame data = sqlContext.load("/spark/rawdata/parquet");
+		// //get from raw data
+		// data.toJavaRDD().foreach(new VoidFunction<Row>() {
+		//
+		// @Override
+		// public void call(Row t) throws Exception {
+		// // TODO Auto-generated method stub
+		// logger.info(t.toString());
+		// }
+		// });
 		sc.stop();
 	}
 
