214a2a48a4a65e9b8ab69f199a7f59a463c5559e
BaoHV <BaoHV@WIN-6ICNIQVFE0A>
2015-04-01 17:11:55 +0700

    dadas

--- WordSearch/src/main/java/vn/wss/wordsearch/WordSearch.java
+++ WordSearch/src/main/java/vn/wss/wordsearch/WordSearch.java
@@ -40,7 +40,7 @@ public class WordSearch {
 				"spark.cassandra.connection.host", "10.0.0.11");
 
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		String fromStr = WordSearch.dateToString(new DateTime(2015, 3, 31, 0,
+		String fromStr = WordSearch.dateToString(new DateTime(2015, 3, 1, 0,
 				0, 0).toDate());
 		String nowStr = WordSearch.dateToString(new DateTime(2015, 3, 31, 23,
 				59, 59).toDate());
@@ -48,20 +48,20 @@ public class WordSearch {
 				.cassandraTable("tracking", "tracking").where(
 						"year_month = ? AND at > ? and at < ?", 201503,
 						fromStr, nowStr);
-		JavaRDD<String> lazada = sc.textFile("/lazada.txt");
+//		JavaRDD<String> lazada = sc.textFile("/lazada.txt");
 		// String url =
 		// "http://websosanh.vn/gionee-gn800-5-5mp-2gb-2-sim-trang/3120163158968901227/direct.htm";
 		System.out.println(rawData.count());
-		JavaPairRDD<String, Integer> lazadaID = lazada
-				.mapToPair(new PairFunction<String, String, Integer>() {
-
-					@Override
-					public Tuple2<String, Integer> call(String t)
-							throws Exception {
-						// TODO Auto-generated method stub
-						return new Tuple2<String, Integer>(t, 1);
-					}
-				});
+//		JavaPairRDD<String, Integer> lazadaID = lazada
+//				.mapToPair(new PairFunction<String, String, Integer>() {
+//
+//					@Override
+//					public Tuple2<String, Integer> call(String t)
+//							throws Exception {
+//						// TODO Auto-generated method stub
+//						return new Tuple2<String, Integer>(t, 1);
+//					}
+//				});
 		long count = rawData
 				.filter(new Function<CassandraRow, Boolean>() {
 
@@ -73,40 +73,40 @@ public class WordSearch {
 						if (uri == null) {
 							return false;
 						}
-						return uri.endsWith("/direct.htm");
-					}
-				})
-				.mapToPair(new PairFunction<CassandraRow, String, Integer>() {
-
-					@Override
-					public Tuple2<String, Integer> call(CassandraRow t)
-							throws Exception {
-						// TODO Auto-generated method stub
-						String[] s = t.getString("uri").split("/");
-						String key = s[s.length - 2];
-						return new Tuple2<String, Integer>(key, 1);
-					}
-				})
-				.reduceByKey(new Function2<Integer, Integer, Integer>() {
-
-					@Override
-					public Integer call(Integer v1, Integer v2)
-							throws Exception {
-						// TODO Auto-generated method stub
-						return v1 + v2;
-					}
-				})
-				.join(lazadaID)
-				.filter(new Function<Tuple2<String, Tuple2<Integer, Integer>>, Boolean>() {
-
-					@Override
-					public Boolean call(
-							Tuple2<String, Tuple2<Integer, Integer>> v1)
-							throws Exception {
-						// TODO Auto-generated method stub
-						return v1._2()._2() == 1;
+						return uri.equals("http://websosanh.vn/may-dieu-hoa-de-ban-mini-loai-1-usa-store-1081-xanh/1976639316872890076/direct.htm");
 					}
 				}).count();
+//				.mapToPair(new PairFunction<CassandraRow, String, Integer>() {
+//
+//					@Override
+//					public Tuple2<String, Integer> call(CassandraRow t)
+//							throws Exception {
+//						// TODO Auto-generated method stub
+//						String[] s = t.getString("uri").split("/");
+//						String key = s[s.length - 2];
+//						return new Tuple2<String, Integer>(key, 1);
+//					}
+//				})
+//				.reduceByKey(new Function2<Integer, Integer, Integer>() {
+//
+//					@Override
+//					public Integer call(Integer v1, Integer v2)
+//							throws Exception {
+//						// TODO Auto-generated method stub
+//						return v1 + v2;
+//					}
+//				})
+//				.join(lazadaID)
+//				.filter(new Function<Tuple2<String, Tuple2<Integer, Integer>>, Boolean>() {
+//
+//					@Override
+//					public Boolean call(
+//							Tuple2<String, Tuple2<Integer, Integer>> v1)
+//							throws Exception {
+//						// TODO Auto-generated method stub
+//						return v1._2()._2() == 1;
+//					}
+//				}).count();
 		logger.info("Lazada click: " + count);
 		sc.stop();
 	}
