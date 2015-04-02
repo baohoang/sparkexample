8d0d6b67a5275eea22e01f184308221ef1e3c29e
BaoHV <BaoHV@WIN-6ICNIQVFE0A>
2015-04-02 09:09:44 +0700

    sdfsd

--- sparkexample/src/main/java/vn/wss/spark/sql/SqlServer.java
+++ sparkexample/src/main/java/vn/wss/spark/sql/SqlServer.java
@@ -11,17 +11,19 @@ import org.apache.spark.sql.SQLContext;
 public class SqlServer {
 	public static void main(String[] args) {
 		SparkConf conf = new SparkConf(true);
+
+		conf.setJars(new String[] { "/home/hdspark/jdbc4.jar" });
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		SQLContext sqlContext = new SQLContext(sc);
 		String url = "jdbc:sqlserver://183.91.14.82;databaseName=QT_2";
-		String username="qt_vn";
-		String password="@F4sJ=l9/ryJt9MT";
-		url+=";username="+username+";password="+password;
-		String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-		Map<String, String> options=new HashMap<>();
+		String username = "qt_vn";
+		String password = "@F4sJ=l9/ryJt9MT";
+		url += ";username=" + username + ";password=" + password;
+		String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+		Map<String, String> options = new HashMap<>();
 		options.put("url", url);
 		options.put("driver", driver);
-		options.put("dbtable","[QT_2].[dbo].[Product_Relation]");
+		options.put("dbtable", "[QT_2].[dbo].[Product_Relation]");
 		DataFrame jdbcDF = sqlContext.load("jdbc", options);
 		jdbcDF.printSchema();
 		sc.stop();
